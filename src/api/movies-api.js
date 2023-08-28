aedd5bf060e989ee02ddf8df5db8abfc7af26f61
LarysaHliieva <lora.glievaya@gmail.com>
2023-08-28 18:28:47 +0300

    update tokin

--- src/api/movies-api.js
+++ src/api/movies-api.js
@@ -2,13 +2,8 @@ import axios from 'axios';
 
 const instance = axios.create({
   baseURL: 'https://api.themoviedb.org/3',
-  // params: {
-  //   api_key: 'aee404d75c4c2c296f37b55a19cb21fa',
-  // },
-
-  headers: {
-    Authorization:
-      'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJhZWU0MDRkNzVjNGMyYzI5NmYzN2I1NWExOWNiMjFmYSIsInN1YiI6IjY0ZTdmNGZmOTBlYTRiMDBlNDlkNGUxNyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.mTc_JNv7EEKXndt1mt3DwFo9CGeoGeDv2NGXGU5Ue3k',
+  params: {
+    api_key: 'aee404d75c4c2c296f37b55a19cb21fa',
   },
 });
 
